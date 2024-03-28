8bf9cea3eb27175f480df9f9950691d45dd7611b
Kal Marsh <kal.marsh@gov.bc.ca>
2024-03-28 11:18:46 -0700

    fixes sync lfp

--- Modules/Lfp/Http/Controllers/LfpController.php
+++ Modules/Lfp/Http/Controllers/LfpController.php
@@ -36,41 +36,35 @@ class LfpController extends Controller
     public function sync($status = true, $newApp = 0)
     {
         //select last app entered
-        $lfp = Lfp::select('id', 'app_idx')->orderBy('created_at', 'desc')->first();
+        $lfp = Lfp::select('id', 'app_idx', 'sin')->orderBy('app_idx', 'asc')->whereNotNull('app_idx')->first();
         $qry = env("LFP_APP_SYNC") . $lfp->app_idx;
         $sfas = DB::connection('oracle')->select($qry);
 
         foreach ($sfas as $app){
-            $check = Lfp::select('id', 'app_idx')->where('app_idx', $app->pl_loan_forgiveness_app_idx)->first();
-            if(is_null($check)){
-                Lfp::firstOrCreate([
+            $check = Lfp::select('id', 'app_idx', 'sin')->where('app_idx', $app->pl_loan_forgiveness_app_idx)->first();
+            if(is_null($check)) {
+                $check = Lfp::firstOrCreate([
                     'sin' => $app->sin,
                     'app_idx' => $app->pl_loan_forgiveness_app_idx,
                 ]);
+            }
 
-                $qry = env("LFP_SFA_APP_PAY") . $lfp->app_idx;
-                $sfas_payments = DB::connection('oracle')->select($qry);
-                foreach($sfas_payments as $spay){
-                    $checkPayment = Payment::where('app_idx', $spay->pl_loan_forgiveness_app_idx)
-                        ->where('pay_idx', $spay->pl_loan_forgiveness_pay_idx)->first();
-                    if(is_null($checkPayment)){
-                        $pay = Payment::firstOrNew(['pay_idx' => $spay->pl_loan_forgiveness_pay_idx]);
-                        $pay->lfp_id = $app->id;
-                        $pay->direct_lend_payment_amount = $spay->pl_dire_principal_pay_amt;
-                        $pay->app_idx = $spay->pl_loan_forgiveness_app_idx;
-                        $pay->pay_idx = $spay->pl_loan_forgiveness_pay_idx;
-                        $pay->save();
-                    }
+            $qry = env("LFP_SFA_APP_PAY") . $app->pl_loan_forgiveness_app_idx;
+            $sfas_payments = DB::connection('oracle')->select($qry);
+            foreach($sfas_payments as $spay){
+                $checkPayment = Payment::where('app_idx', $spay->pl_loan_forgiveness_app_idx)
+                    ->where('pay_idx', $spay->pl_loan_forgiveness_pay_idx)->count();
+                if($checkPayment === 0){
+                    $pay = Payment::firstOrNew(['pay_idx' => $spay->pl_loan_forgiveness_pay_idx]);
+                    $pay->lfp_id = $check->id;
+                    $pay->app_idx = $spay->pl_loan_forgiveness_app_idx;
+                    $pay->pay_idx = $spay->pl_loan_forgiveness_pay_idx;
+                    $pay->save();
                 }
             }
         }
-//
-//        $lfps = new Lfp();
-//        $lfps = $this->paginateLfps($lfps);
 
-        return Redirect::route('lfp.applications');
-
-//        return Inertia::render('Lfp::Applications', ['page' => 'applications', 'status' => $status, 'results' => $lfps, 'app' => $newApp]);
+        return Redirect::route('lfp.applications.index');
     }
 
     /**
