8244a10e20541e0a662090c4b9431f8d5651ea54
Kal Marsh <kal.marsh@gov.bc.ca>
2024-05-13 09:35:55 -0700

    adds new fields for proposed payment info for LFP

--- Modules/Plsc/Database/Migrations/2024_05_01_154308_create_applications_table.php
+++ Modules/Plsc/Database/Migrations/2024_05_01_154308_create_applications_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class CreatePlscApplicationsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -80,4 +80,4 @@ class CreatePlscApplicationsTable extends Migration
     {
         Schema::connection(env('DB_DATABASE_PLSC'))->dropIfExists('applications');
     }
-}
+};
