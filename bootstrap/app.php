b644a9ed91cbc3e393d9680b21057ffa9641752d
Mael Cormier <mael.cormier@cgi.com>
2025-07-18 09:51:39 -0700

    Migrate to Laravel 12 middleware configuration
    - Remove deprecated Http/Kernel.php files (replaced by bootstrap/app.php)
    - Update bootstrap/app.php with new Laravel 12 middleware configuration
    - Migrate middleware aliases from Kernel::$middlewareAliases to app.php
    - Add module-specific middleware aliases (yeaf_active, twp_active, etc.)
    - Replace legacy middleware registration with new withMiddleware() method
    - Fix Inertia.js compatibility with new middleware structure
    
    Kernel.php configuration moved to bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,55 +1,98 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-$app = new Illuminate\Foundation\Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-);
+return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        // Application Service Providers
+        App\Providers\AppServiceProvider::class,
+        App\Providers\AuthServiceProvider::class,
+        App\Providers\EventServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
 
-/*
-|--------------------------------------------------------------------------
-| Bind Important Interfaces
-|--------------------------------------------------------------------------
-|
-| Next, we need to bind some important interfaces into the container so
-| we will be able to resolve them when needed. The kernels serve the
-| incoming requests to this application from both the web and CLI.
-|
-*/
+        // Lfp Module
+        Modules\Lfp\Providers\LfpServiceProvider::class,
+        Modules\Lfp\Providers\RouteServiceProvider::class,
 
-$app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
-);
+        // Twp Module
+        Modules\Twp\Providers\TwpServiceProvider::class,
+        Modules\Twp\Providers\RouteServiceProvider::class,
 
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
-);
+        // Vss Module
+        Modules\Vss\Providers\VssServiceProvider::class,
+        Modules\Vss\Providers\RouteServiceProvider::class,
 
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
-);
+        // Neb Module
+        Modules\Neb\Providers\NebServiceProvider::class,
+        Modules\Neb\Providers\RouteServiceProvider::class,
 
-/*
-|--------------------------------------------------------------------------
-| Return The Application
-|--------------------------------------------------------------------------
-|
-| This script returns the application instance. The instance is given to
-| the calling script so we can separate the building of the instances
-| from the actual running of the application and sending responses.
-|
-*/
+        // Yeaf Module
+        Modules\Yeaf\Providers\YeafServiceProvider::class,
+        Modules\Yeaf\Providers\RouteServiceProvider::class,
 
-return $app;
+        // Plsc Module
+        Modules\Plsc\Providers\PlscServiceProvider::class,
+        Modules\Plsc\Providers\RouteServiceProvider::class,
+
+        // Package Service Providers
+        Barryvdh\DomPDF\ServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+    ])
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->web(append: [
+            \App\Http\Middleware\HandleInertiaRequests::class,
+            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+        ]);
+
+        // Modules middlewares
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+            'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+            'can' => \Illuminate\Auth\Middleware\Authorize::class,
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+            'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+            'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
+            'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+            'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+            // YEAF
+            'yeaf_admin' => \Modules\Yeaf\Http\Middleware\IsAdmin::class,
+            'yeaf_active' => \Modules\Yeaf\Http\Middleware\IsActive::class,
+
+            // TWP
+            'twp_inertia' => \Modules\Twp\Http\Middleware\HandleInertiaRequests::class,
+            'twp_admin' => \Modules\Twp\Http\Middleware\IsAdmin::class,
+            'twp_active' => \Modules\Twp\Http\Middleware\IsActive::class,
+
+            // VSS
+            'vss_admin' => \Modules\Vss\Http\Middleware\IsAdmin::class,
+            'vss_active' => \Modules\Vss\Http\Middleware\IsActive::class,
+
+            // NEB
+            'neb_admin' => \Modules\Neb\Http\Middleware\IsAdmin::class,
+            'neb_active' => \Modules\Neb\Http\Middleware\IsActive::class,
+
+            // LFP
+            'lfp_admin' => \Modules\Lfp\Http\Middleware\IsAdmin::class,
+            'lfp_active' => \Modules\Lfp\Http\Middleware\IsActive::class,
+
+            // PLSC
+            'plsc_admin' => \Modules\Plsc\Http\Middleware\IsAdmin::class,
+            'plsc_active' => \Modules\Plsc\Http\Middleware\IsActive::class,
+
+            'super_admin' => \App\Http\Middleware\SuperAdmin::class,
+            'apiauth' => \App\Http\Middleware\ApiAuth::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
