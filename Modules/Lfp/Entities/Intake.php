26dc9b8bf5eb00b7b64d999175a2e57b3a46d59e
Kal Marsh <kal.marsh@gov.bc.ca>
2025-08-19 13:20:46 -0700

    adds column for inactive INFT apps. adds export to intakes

--- Modules/Lfp/Entities/Intake.php
+++ Modules/Lfp/Entities/Intake.php
@@ -15,7 +15,8 @@ class Intake extends ModuleModel
     protected $fillable = [
         'sin', 'first_name', 'last_name', 'profession', 'employer', 'employment_status', 'community', 'repayment_status',
         'in_good_standing', 'repayment_start_date', 'amount_owing', 'intake_status', 'receive_date', 'comment',
-        'proposed_registration_date', 'denial_reason', 'app_idx', 'alias_name',
+        'proposed_registration_date', 'denial_reason', 'app_idx', 'alias_name', 
+        'both_eligibility_status',
     ];
 
     public function lfp()
