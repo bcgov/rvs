fe9082bd1fc154ace63d44e770ffedbc8c30f898
Kal Marsh <kal.marsh@gov.bc.ca>
2025-08-25 15:13:58 -0700

    fixes api access response

--- routes/api2.php
+++ routes/api2.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::group(['middleware' => 'apiauth'], function (): void {
-    Route::post('/fetch', [ServiceAccountController::class, 'fetchData']);
-    Route::post('/tables', [ServiceAccountController::class, 'fetchTables']);
-    Route::post('/columns', [ServiceAccountController::class, 'fetchColumns']);
-});
+// Route::group(['middleware' => 'apiauth'], function (): void {
+//     Route::post('/fetch', [ServiceAccountController::class, 'fetchData']);
+//     Route::post('/tables', [ServiceAccountController::class, 'fetchTables']);
+//     Route::post('/columns', [ServiceAccountController::class, 'fetchColumns']);
+// });
